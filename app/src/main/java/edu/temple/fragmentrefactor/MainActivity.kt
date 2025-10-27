943aaf41b07553abefa340c7976f7a28cbd49ce5
James McMichael <tur08816@temple.edu>
2025-10-27 15:37:48 -0400

    moved functionality to a fragment

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -11,18 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val changeButton = findViewById<Button>(R.id.changeButton)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, MainFragment())
+            .commit()
 
-        changeButton.setOnClickListener {
-            val name = nameEditText.text
-
-            displayTextView.text = if (name.isNotBlank()) {
-                "Hello, $name!"
-            } else {
-                "Please enter your name"
-            }
-        }
     }
 }
---
+++ app/src/main/java/edu/temple/fragmentrefactor/MainFragment.kt
@@ -0,0 +1,63 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class MainFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false).apply {
+            var display = findViewById<TextView>(R.id.displayTextView)
+            var name = findViewById<TextView>(R.id.nameEditText)
+            findViewById<Button>(R.id.changeButton).setOnClickListener {
+                display.text = "hello, ${name.text}!!"
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
